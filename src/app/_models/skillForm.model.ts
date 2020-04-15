95c74e46a4121d4cc0e9a2669adc3b61369501b7
ZygmuntBuszyk <zyrgis@gmail.com>
2020-04-15 22:11:41 +0200

    added add skill form functionality --front

---
+++ src/app/_models/skillForm.model.ts
@@ -0,0 +1,8 @@
+export class SkillForm {
+    name: string;
+    parameters: SkillFormName[];   
+}
+
+export class SkillFormName {
+    name: string
+}
--- src/app/modules/skills/_common/_components/add-skill-modal/add-skill-modal.component.ts
+++ src/app/modules/skills/_common/_components/add-skill-modal/add-skill-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { SkillForm, SkillFormName } from 'src/app/_models/skillForm.model'
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-add-skill-modal',
@@ -8,20 +10,51 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AddSkillModalComponent implements OnInit {
 
+
+
+  constructor(private fb: FormBuilder,private ngxSmartModalService: NgxSmartModalService) {
+  }
+
   addForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  testForm: FormGroup;
+
+  success = false;
+  loading = false;
 
   ngOnInit(): void {
     this.addForm = this.fb.group({
-      name: ''
-    });
-
-    this.addForm.valueChanges.subscribe(
-      values => {
-        console.log(values)
-      }
-    )
+      name: ['', [Validators.minLength(2), Validators.required]],
+      formParameters: this.fb.array([this.groupParameter])
+    }); 
   }
 
+  get getParameters() {
+    return this.addForm.get('formParameters') as FormArray;
+  }
+
+  addParameter() {
+    this.getParameters.push(this.groupParameter);
+  }
+
+  deleteParameter(index) {
+    this.getParameters.removeAt(index);
+  }
+
+  get groupParameter() {
+    return this.fb.group({name: ['',[ Validators.minLength(2), Validators.required]]});
+  }
+  
+ submitHandler() {
+    console.log('added');
+    
+    this.success = true;
+    this.loading = false;
+    this.addForm = this.fb.group({
+      name: ['', [Validators.minLength(2), Validators.required]],
+      formParameters: this.fb.array([this.groupParameter])
+    }); 
+    this.ngxSmartModalService.getModal('addSkillModal').close();
+
+  }
 }
